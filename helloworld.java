7f0c5e2ded0a205da1027bbbf3f946ea874b62e5
phanindra <phanindra032@gmail.com>
2017-09-15 21:03:37 -0400

    tgis is new commit

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,8 @@
 # This is the first programme
 // this is the first edit done after creating a file
 	public class HelloWorld {
-# added a new comment line for fourth step
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      System.out.println("Hello, GIT World");
    }
 }
