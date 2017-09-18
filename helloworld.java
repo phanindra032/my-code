a6620dd5e8e63eaae8ccf4df9d43f01b3bf9e8d1
phanindra <phanindra032@gmail.com>
2017-09-17 22:58:12 -0400

    having only two logs

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,9 @@
 # This is the first programme
+// this is the first edit done after creating a file
 	public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+     // end of the printf commands
    }
 }
